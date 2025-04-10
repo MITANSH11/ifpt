cbab4dbbd8c3e29e0a99e76e2889d8ddff6be8de
Mitansh11 <mitanshprajapati1110@gmail.com>
2025-04-10 14:36:05 +0530

    jar

---
+++ IFPT/CalcAppFX/src/main/java/calc/calcappfx/Launcher.java
@@ -0,0 +1,9 @@
+package calc.calcappfx;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(mainApp.class, args);
+    }
+}
